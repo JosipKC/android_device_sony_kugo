a87276b290b0016a94109ab53155e43262333680
JosipKC <d3hum4niz3d@gmail.com>
2020-01-28 12:33:32 +0100

    add device specific props

---
+++ vendor_prop.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# SEMC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.semc.product.model=F5321 \
+    ro.semc.version.fs=GENERIC \
+    ro.semc.version.fs_revision=34.4.A.2.118 \
+    ro.semc.version.sw=1304-9007 \
+    ro.semc.version.sw_revision=34.4.A.2.118 \
+    ro.semc.version.sw_type=user \
+    ro.semc.version.sw_variant=R1E
